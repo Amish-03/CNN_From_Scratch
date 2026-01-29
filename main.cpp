94c85e393ad9024547ef54d28158b22f093bcb1a
Amish-03 <amishkulkarni03@gmail.com>
2026-01-29 16:34:38 +0530

    Update main.cpp to test Tensor addition with shared_ptr

--- main.cpp
+++ main.cpp
@@ -7,7 +7,12 @@ using namespace std;
 int main() {
   vector<vector<float>> test_v{
       {1.0, 2.0, 3.0, 4.0}, {1.0, 2.0, 3.0, 4.0}, {1.0, 2.0, 3.0, 4.0}};
-  Tensor test_tensor(test_v);
-  cout << test_tensor << endl;
+  vector<vector<float>> test_v2{
+      {1.0, 2.0, 3.0, 4.0}, {1.0, 2.0, 3.0, 4.0}, {1.0, 2.0, 3.0, 4.0}};
+  shared_ptr<Tensor> test_tensor = make_shared<Tensor>(test_v);
+  shared_ptr<Tensor> test_tensor2 = make_shared<Tensor>(test_v2);
+  cout << *test_tensor << endl;
+  cout << *test_tensor2 << endl;
+  cout << *((*test_tensor) + test_tensor2) << endl;
   return 0;
 }
